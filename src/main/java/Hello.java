2f045fcce5f92d070867b5af8c2a6445ec9ff7c9
Justin Conger <jhconger@GMAIL.COM>
2022-03-30 10:00:51 -0500

    setting up ad lister

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,6 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet(name = "Hello", urlPatterns = "/hello")
@@ -13,14 +10,18 @@ public class Hello extends HttpServlet {
 
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-
         String name = req.getParameter("name");
-        out.println("<HTML>");
-        out.println("<HEAD><TITLE>Hello, " + name + "</TITLE></HEAD>");
-        out.println("<BODY>");
-        out.println("<h1>Hello, " + name + "</h1>");
-        out.println("</BODY></HTML>");
-
+        if (name == null) {
+//        ((paramName = req.getParameterValues("name"))
+//          /**/      != null) {
+            out.println("<h1>Hello, World!</h1>");
+        } else {
+            out.println("<HTML>");
+            out.println("<HEAD><TITLE>Hello, " + name + "</TITLE></HEAD>");
+            out.println("<BODY>");
+            out.println("<h1>Hello, " + name + "</h1>");
+            out.println("</BODY></HTML>");
+        }
     }
 
     public String getServletInfo() {
