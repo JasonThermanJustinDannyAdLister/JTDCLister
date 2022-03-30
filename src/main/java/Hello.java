f0c5da494690d350c0f7d1fd1e6e8a5672b2aa07
Justin Conger <jhconger@GMAIL.COM>
2022-03-30 09:37:07 -0500

    completed bonus

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Hello {
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "Hello", urlPatterns = "/hello")
+public class Hello extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        String name = req.getParameter("name");
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE>Hello, " + name + "</TITLE></HEAD>");
+        out.println("<BODY>");
+        out.println("Hello, " + name);
+        out.println("</BODY></HTML>");
+    }
+
+    public String getServletInfo() {
+        return "A servlet that knows the name of the person to whom it's" +
+                "saying hello";
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        doGet(req, res);
+    }
 }
