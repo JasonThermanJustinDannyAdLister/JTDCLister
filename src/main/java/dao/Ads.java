db7b540f1a9b8f3c7df26233f5c7acbb91ee8ff5
JMComstock <jasoncomstock11@gmail.com>
2022-04-15 11:40:39 -0500

    added working delete function

--- src/main/java/dao/Ads.java
+++ src/main/java/dao/Ads.java
@@ -3,19 +3,28 @@
 package dao;
 
 import models.Ad;
-import models.User;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface Ads {
     List<Ad> all();
+
     long insert(Ad ad);
+
     Ad get(long i);
+
     Ad findById(long id);
+
     List<Ad> allUserAds(long currentUserId);
+
+    void delete(Long id);
+
     void edit( Ad newAd);
+
     List<Ad> sortAds(long id);
+
     List<Ad> sortAdsAscending(long id);
+
     List<Ad> searchAdsFromResults(String searchAds) throws SQLException;
 }
